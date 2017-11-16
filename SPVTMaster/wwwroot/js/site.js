b5c41c5c1b7040c9e1be70dc8e9bc64f8f169e4f
Mikelawson114 <Mikelawson114@outlook.com>
2017-11-16 02:20:34 -0700

    Easter Egg!

--- SPVTMaster/wwwroot/js/site.js
+++ SPVTMaster/wwwroot/js/site.js
@@ -1,23 +1,27 @@
 ﻿// Write your JavaScript code.
 function validation() {
     var make = document.getElementById("make").value;
-    if (make === "" || make == null) {
+    var model = document.getElementById("model").value;
+    var color = document.getElementById("color").value;
+    var lpn = document.getElementById("lpn").value;
+    var vtype = document.getElementById("vtype").value;
+
+    if (make === "" && model === "" && color === "" && lpn === "" && vtype === "") {
+        alert("DON'T BE A BANAN!!!!")
+    }
+    else if (make === "" || make == null) {
         alert("Make input must be filled out!")
     }
-    var model = document.getElementById("model").value;
-    if (model === "" || model == null) {
+    else if (model === "" || model == null) {
         alert("Model input must be filled out!")
     }
-    var color = document.getElementById("color").value;
-    if (color === "" || color == null) {
+    else if (color === "" || color == null) {
         alert("Color input must be filled out!")
     }
-    var lpn = document.getElementById("lpn").value;
-    if (lpn === "" || lpn == null) {
+    else if (lpn === "" || lpn == null) {
         alert("License Plate input must be filled out!")
-    } 
-    var vtype = document.getElementById("vtype").value;
-    if (vtype === "" || vtype == null) {
+    }
+    else if (vtype === "" || vtype == null) {
         alert("Violation Type input must be filled out!")
     }
 }
